69b673c157802a42851c646d63189dbf352a671f
tuxburner <sebasth@gmx.de>
2014-09-10 01:18:06 +0200

    Express is bitchi with // :(

--- scripts/main.js
+++ scripts/main.js
@@ -18,7 +18,7 @@ $(function() {
       "back" : 27,
       "action" : 13
     },
-    "backenUrl" : "http://192.168.0.150:8080/",
+    "backenUrl" : "http://192.168.0.150:8080",
     "sleepTimeOut" : 10
   }
 
