a55e015a88a1daa691a84561de29c3fd4aece7dc
tuxburner <sebasth@gmx.de>
2014-09-17 11:42:12 +0200

    Turned on rotary

--- scripts/main.js
+++ scripts/main.js
@@ -18,7 +18,7 @@ $(function() {
       "back" : 27,
       "action" : 13
     },
-    "rotaryInput" : false,
+    "rotaryInput" : true,
     "rotaryEventTimeout" : 750,
     "backenUrl" : "http://localhost:8080",
     "sleepTimeOut" : 600
