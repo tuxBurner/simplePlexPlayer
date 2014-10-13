3c3b951df12384b85e2188cf61f2b908741d1a02
tuxburner <sebasth@gmx.de>
2014-10-13 10:57:46 +0200

    saved some stuff from the rpi

--- startPlayer.sh
+++ startPlayer.sh
@@ -4,5 +4,14 @@ matchbox-window-manager & :
 xset -dpms
 xset s off
 while true; do
+cd backend
+./node-v0.10.2-linux-arm-pi/bin/node server.js &
+cd ..
+
+cd backend/pikeyd
+./pikeyd -k
+./pikeyd -pu -d
+cd ../..
+
 /usr/bin/midori -c ~/.config/midori -e Fullscreen -a ./index.html
 done
