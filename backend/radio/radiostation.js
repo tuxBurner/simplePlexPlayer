4de1d3987735cd3c59b6e3ebae386586b97bc4ff
tuxburner <sebasth@gmx.de>
2014-11-11 08:44:51 +0100

    Added some sample radio stations

---
+++ backend/radio/radiostation.js
@@ -0,0 +1,9 @@
+[{
+  "name": "radiobob",
+  "url": "http://streams.radiobob.de/bob-live/mp3-192/mediaplayerbob",
+  "thumb": "600x600_bob-live_colored.png"
+}, {
+  "name": "wackenradio",
+  "url": "http://wackenradio-high.rautemusik.fm",
+  "thumb": "shp_5339899c5d7d5"
+}]
