ea7661eebda6bcfd00dffc21fc8c8f47e2984572
Sebastian Hardt <tuxburner@boggo.de>
2014-11-18 08:13:42 +0100

    Added radiosation support

--- backend/RadioSource.js
+++ backend/RadioSource.js
@@ -1,4 +1,5 @@
 var Folder = require('./Folder.js');
+var AudioFile = require('./AudioFile.js');
 var fs = require('fs');
 
 /**
@@ -11,10 +12,19 @@ function RadioSource(conf) {
   this.rootFolder = new Folder(conf.name, conf.path);
 
   this.loadSource = function() {
-    if (fs.existsSync(conf.path) == false) {
+    if (fs.existsSync(conf.cfgFile) == false) {
       throw new Error("Radio cfg file: " + conf.cfgFile + " does not exist");
     }
 
+    var stationsCfg = require(conf.cfgFile);
+
+    for (idx in stationsCfg) {
+      var station = stationsCfg[idx];
+      var file = new AudioFile(station.name, station.url, station.thumb,
+        null);
+      file.stream = true;
+      this.rootFolder.addFile(file);
+    }
   }
 
   // initialze the radio stations
