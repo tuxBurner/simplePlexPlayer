ecd9da159e2d5bdd3113cbbfecbeef25032659d1
Sebastian Hardt <tuxburner@boggo.de>
2015-02-28 18:13:55 +0100

    Started finishing options menu

--- frontend/scripts/player/MenuItem.js
+++ frontend/scripts/player/MenuItem.js
@@ -1,107 +1,42 @@
-var MenuTools = function() {}
-MenuTools.loadSourceEntrances = function(id, highlightId) {
-	var url = "/sources/" + id.split('?').join('%3F');
-	url = Config.baseUrl + url;
-	Tools.callBackend(url, function(data) {
-		var menuItems = [];
-
-		// check if we only have one audio menu
-		if (data.subFolders.length == 0 && data.audioFiles.length == 1) {
-			menuItems.push(new PlayerMenuItem(data.audioFiles[0], id + "/" + data.audioFiles[0].name));
-			MenuHandler.setCurrentItems(menuItems, highlightId);
-			return;
-		}
-
-		for (idx in data.subFolders) {
-			var subFolder = data.subFolders[idx];
-			menuItems.push(new DirectoryMenuItem(subFolder.name, subFolder.thumb, id));
-		}
-
-		if (data.audioFiles.length > 1) {
-			menuItems.push(new AudioAllFilesMenuItem(id, data.audioFiles));
-		}
-
-		for (idx in data.audioFiles) {
-			var audioFile = data.audioFiles[idx];
-			menuItems.push(new AudioFileMenuItem(audioFile, id));
-		}
-
-		MenuHandler.setCurrentItems(menuItems, highlightId);
-	});
-}
-
-MenuTools.displayMenuItem = function(menuItem) {
-	player.displayContent("menuitem", {
-		"menuItem": menuItem,
-		"menuStack": MenuHandler.menuStack,
-		"position": MenuHandler.currentMenuIdx + 1,
-		"itemsCount": MenuHandler.currentMenuItems.length
-	});
-}
-
-MenuTools.mainMenuItems = null;
-MenuTools.loadMainMenu = function(hilightId) {
-
-	if (MenuTools.mainMenuItems == null) {
-
-		MenuTools.mainMenuItems = [];
-		// load all the sources from the backend
-		Tools.callBackend(Config.baseUrl + "/sources", function(data) {
-			for (idx in data) {
-				var source = data[idx];
-				MenuTools.mainMenuItems.push(new SourceMenuItem(source));
-			}
-
-			// finaly add the options main menu
-			MenuTools.mainMenuItems.push(new OptionMainMenuItem());
-			MenuHandler.setCurrentItems(MenuTools.mainMenuItems);
-
-		});
-	} else {
-		MenuHandler.setCurrentItems(MenuTools.mainMenuItems, hilightId);
-	}
-}
-
-
 /**
  * This is the simpliest menu item we can have
  */
 var MenuItem = function() {
-	this.title = "No title set";
-	this.id = "No id set";
-	this.cssClass = "";
+  this.title = "No title set";
+  this.id = "No id set";
+  this.cssClass = "";
 
-	// normal handling of actions take place here
-	this.handleKeyEvent = function(actionType) {
-		if (actionType == "left") {
-			MenuHandler.displayNextItem(false);
-		}
-		if (actionType == "right") {
-			MenuHandler.displayNextItem(true);
-		}
-		if (actionType == "action") {
-			this.performAction();
-		}
+  // normal handling of actions take place here
+  this.handleKeyEvent = function(actionType) {
+    if (actionType == "left") {
+      MenuHandler.displayNextItem(false);
+    }
+    if (actionType == "right") {
+      MenuHandler.displayNextItem(true);
+    }
+    if (actionType == "action") {
+      this.performAction();
+    }
 
-		if (actionType == "back") {
-			this.performBack();
-		}
-	}
+    if (actionType == "back") {
+      this.performBack();
+    }
+  }
 
-	this.performAction = function() {
-		alert("Override me performAction");
-	}
+  this.performAction = function() {
+    alert("Override me performAction");
+  }
 
-	this.performBack = function() {
-		MenuHandler.loadParentContent();
-	}
+  this.performBack = function() {
+    MenuHandler.loadParentContent();
+  }
 
-	// this is called when the menu item is displaying its content
-	this.displayContent = function() {
-		alert("Override me displayContent !");
-	}
+  // this is called when the menu item is displaying its content
+  this.displayContent = function() {
+    alert("Override me displayContent !");
+  }
 
-	this.loadSubMenuItems = function(highlightId) {
-		alert("Override me loadSubMenuItems !");
-	}
+  this.loadSubMenuItems = function(highlightId) {
+    alert("Override me loadSubMenuItems !");
+  }
 }
--- frontend/scripts/player/Player.js
+++ frontend/scripts/player/Player.js
@@ -1,100 +1,58 @@
 var Player = function(config) {
 
-	var that = this;
+  var that = this;
 
-	this.templates = {};
+  this.templates = {};
 
-	//this.audioJsWrapper = new AudioJsWrapper(config.skipSeconds, config.repeatAll);
+  //this.audioJsWrapper = new AudioJsWrapper(config.skipSeconds, config.repeatAll);
 
-	this.config = config;
-
-
-	this.currentDisplayTpl = ""
+  this.config = config;
 
 
-	this.keyBoardHandler = new KeyBoardEventHandler(that);
-	this.optionsHandler = new OptionsHandler(that);
-
-	this.timeOut = null;
-
-	this.displayOff = false;
+  this.currentDisplayTpl = ""
 
 
-	/**
-	 * Initializes the player
-	 */
-	this.init = function() {
-		that.initTemplats();
+  this.keyBoardHandler = new KeyBoardEventHandler(that);
+  this.optionsHandler = new OptionsHandler(that);
 
-		MenuTools.loadMainMenu();
-		if (that.config.sleepTimeOut !== undefined) {
-			that.startTimeOut();
-		}
-	}
+  this.timeOut = null;
+
+  this.displayOff = false;
 
 
-	/**
-	 * Initializes the handlebar templates and caches them
-	 */
-	this.initTemplats = function() {
-		that.templates = {
-			"mainMenu": Handlebars.compile($("#main-tpl").html()),
-			"player": Handlebars.compile($("#player-tpl").html()),
-			"menuitem": Handlebars.compile($("#menuitem-tpl").html()),
-			// templates for the options
-			"options_wrapper": Handlebars.compile($("#options_wrapper-tpl").html()),
-			"options_sysinfos": Handlebars.compile($("#options_sysinfos-tpl").html()),
-			"options_wifisettings": Handlebars.compile($(
-				"#options_wifisettings-tpl").html())
-		}
-	}
+  /**
+   * Initializes the player
+   */
+  this.init = function() {
+
+
+    MenuTools.loadMainMenu();
+    if (that.config.sleepTimeOut !== undefined) {
+      that.startTimeOut();
+    }
+  }
 
 
 
-	this.displayContent = function(tplName, data, callBack) {
-		var content = that.templates[tplName](data);
-		that.currentDisplayTpl = tplName;
-		$('#mainContainer').html(content);
-		if (callBack !== undefined) {
-			callBack();
-		}
-	}
+  this.startTimeOut = function() {
+    if (that.displayOff == true) {
+      // make sure the display is on
+      $.get(that.config.baseUrl + "/display/on");
+    }
 
+    that.displayOff = false;
 
-	/**
-	 * Display the current options
-	 */
-	this.displayOptions = function(currentMenuItemId) {
-		// we just want to display the option menu items ?
-		if (currentMenuItemId == "opts_main") {
-			that.menuHandler.initOptionMenu();
-			that.displayContent("mainMenu", {}, function() {
-				that.initMenu()
-			});
-		} else {
-			that.optionsHandler.getOptionsMenuItems(currentMenuItemId);
-		}
-	}
+    clearTimeout(that.timeOut)
+    that.timeOut = setTimeout(that.handleTimeOut, that.config.sleepTimeOut *
+      1000);
+  }
 
-	this.startTimeOut = function() {
-		if (that.displayOff == true) {
-			// make sure the display is on
-			$.get(that.config.baseUrl + "/display/on");
-		}
+  this.handleTimeOut = function() {
+    that.displayOff = true;
 
-		that.displayOff = false;
-
-		clearTimeout(that.timeOut)
-		that.timeOut = setTimeout(that.handleTimeOut, that.config.sleepTimeOut *
-			1000);
-	}
-
-	this.handleTimeOut = function() {
-		that.displayOff = true;
-
-		// turn off the display
-		$.get(that.config.baseUrl + "/display/off");
-	}
+    // turn off the display
+    $.get(that.config.baseUrl + "/display/off");
+  }
 
 
 }
