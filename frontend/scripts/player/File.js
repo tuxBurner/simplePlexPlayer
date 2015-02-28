1617998dd05b10d530465ad65797d31f4f4b5acf
Sebastian Hardt <tuxburner@boggo.de>
2015-02-28 15:51:04 +0100

    Player as menu begin

--- frontend/scripts/player/File.js
+++
@@ -1,7 +0,0 @@
-var AudioFile = function(id,title,mp3,thumb,duration) {
-  this.id = id;
-  this.title = title;
-  this.thumb = thumb;
-  this.mp3 = mp3;
-  this.duration = duration;
-}
--- frontend/scripts/player/Player.js
+++ frontend/scripts/player/Player.js
@@ -51,14 +51,6 @@ var Player = function(config) {
 
 
 
-	this.displayPlayer = function(files, title) {
-		that.displayContent("player", {
-			"files": files,
-			"title": title,
-			"menuStack": that.menuHandler.menuStack
-		}, that.initPlayer);
-	}
-
 	this.displayContent = function(tplName, data, callBack) {
 		var content = that.templates[tplName](data);
 		that.currentDisplayTpl = tplName;
@@ -68,111 +60,6 @@ var Player = function(config) {
 		}
 	}
 
-
-	/**
-	 * Performs an action on the current menu item
-	 */
-	/*	this.performAction = function() {
-			var higlightedMenu = that.menuHandler.getCurrentMenuItem();
-
-			var currentMenuItemType = higlightedMenu.type;
-			var currentMenuItemId = higlightedMenu.id;
-
-			that.menuHandler.addMenuItemToStack(higlightedMenu);
-
-			if (currentMenuItemType == "section") {
-				that.loadSection(currentMenuItemId);
-			}
-
-			if (currentMenuItemType == "directory") {
-				that.loadDirectory(currentMenuItemId, false);
-			}
-
-			if (currentMenuItemType == "file") {
-				var files = [];
-				files.push(that.files[currentMenuItemId]);
-				that.displayPlayer(files, that.files[currentMenuItemId].title);
-			}
-
-			if (currentMenuItemType == "playall") {
-				var files = that.directories[currentMenuItemId].files;
-				that.displayPlayer(files, that.directories[currentMenuItemId].title);
-			}
-
-			if (currentMenuItemType == "options") {
-				that.displayOptions(currentMenuItemId);
-			}
-		}*/
-
-
-	/**
-	 * Performs an escap action on the current menu item
-	 */
-	/*this.performEscAction = function() {
-		var removedMenuItem = that.menuHandler.removeLastMenuItemFromStack();
-		if (removedMenuItem == null) {
-			return;
-		}
-
-		that.audioJsWrapper.stop();
-
-		switch (removedMenuItem.type) {
-			case "playall":
-				that.loadDirectory(removedMenuItem.id, false);
-				break;
-			case "file":
-			case "directory":
-				that.loadDirectory(removedMenuItem.parent, false, removedMenuItem.id);
-				break;
-			case "options":
-				window.location.hash = '';
-				that.displayMainMenu();
-				break;
-			default:
-				window.location.hash = '';
-				that.displayMainMenu();
-		}
-	}*/
-
-	this.initPlayer = function() {
-		// Mark the first track
-		$('#playList li').first().addClass('playing');
-		that.audioJsWrapper.loadTrack();
-	}
-
-	//this.loadSection = function(id) {
-	//	that.loadDirectory(id, true);
-	//}
-
-	/**
-	 * Load directory from sever
-	 */
-	/*this.loadDirectory = function(id, section, highlightMenuItem) {
-		var url = "/sources/" + id.split('?').join('%3F');
-
-		url = that.config.baseUrl + url;
-
-		var dir = that.directories[id];
-
-		Tools.callBackend(url, function(data) {
-
-			dir.cleanSubs();
-
-			for (idx in data.subFolders) {
-				var subFolder = data.subFolders[idx];
-				dir.addSubDir(subFolder.name, subFolder.thumb, subFolder.disableCaching,
-					that);
-			}
-
-			for (idx in data.audioFiles) {
-				var audioFile = data.audioFiles[idx];
-				dir.addFile(audioFile, that);
-			}
-
-			that.displayDir(dir, highlightMenuItem);
-		});
-
-	}*/
 
 	/**
 	 * Display the current options
@@ -187,13 +74,6 @@ var Player = function(config) {
 		} else {
 			that.optionsHandler.getOptionsMenuItems(currentMenuItemId);
 		}
-	}
-
-	this.displayDir = function(directory, highlightMenuItem) {
-		that.menuHandler.initDirectoryMenu(directory);
-		that.displayContent("mainMenu", {}, function() {
-			that.initMenu(highlightMenuItem)
-		});
 	}
 
 	this.startTimeOut = function() {
