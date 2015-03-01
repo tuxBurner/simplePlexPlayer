765f14fa493669bc5e56cb8e18f10244b7b75ef1
Sebastian Hardt <tuxburner@boggo.de>
2015-03-01 16:25:49 +0100

    Looki looki

--- frontend/scripts/player/AudioJsWrapper.js
+++ frontend/scripts/player/AudioJsWrapper.js
@@ -1,6 +1,7 @@
 var AudioPlayer = function() {};
 AudioPlayer.audioJs;
 
+
 AudioPlayer.init = function() {
 	AudioPlayer.audioJs = audiojs.create(document.getElementById('audioJsAudio'), {
 		trackEnded: function() {
@@ -13,16 +14,18 @@ AudioPlayer.init = function() {
 }
 AudioPlayer.init();
 
-AudioPlayer.loadTrack = function(streamUrl, startPlaying) {
-	//var trackSrc = $('#playList li.playing').attr('data-src');
+AudioPlayer.loadTrack = function(streamUrl) {
 	AudioPlayer.audioJs.load(streamUrl);
-	//if (startPlaying === undefined || startPlaying == true) {
 	AudioPlayer.audioJs.play();
-	//}
 }
 
 AudioPlayer.updatePercentage = function(percentage) {
+	if ($('#playerDuration').html() == "") {
+		$('#playerDuration').html(Tools.readableDuration(this.audioJs.element.duration));
+	}
 
+	var playedString = Tools.readableDuration(this.audioJs.duration * percentage);
+	$('#playerPlaytime').text(playedString);
 }
 
 AudioPlayer.stop = function() {
@@ -32,93 +35,6 @@ AudioPlayer.stop = function() {
 }
 
 var AudioJsWrapperOld = function(skipSeconds, repeatAll) {
-
-	this.skipSeconds = skipSeconds;
-
-	// repeat playlist when rech end ?
-	this.repeatAll = repeatAll;
-
-	var that = this;
-
-	// initialize the player
-	this.audioJs = audiojs.create(document.getElementById('audioJsAudio'), {
-		trackEnded: function() {
-			that.loadNextTrack(true, that.repeatAll);
-		},
-		updatePlayhead: function(percentage) {
-			that.updatePercentage(percentage);
-		}
-	});
-
-	this.total = undefined;
-
-	this.loadTrack = function(startPlaying) {
-		var trackSrc = $('#playList li.playing').attr('data-src');
-		this.audioJs.load(trackSrc);
-		if (startPlaying === undefined || startPlaying == true) {
-			this.audioJs.play();
-		}
-	}
-
-	this.loadNextTrack = function(nextTitle, repeatOnEnd) {
-		var next = null;
-
-		var playNextTrack = true;
-
-		var scrollPos = $('#playListWrapper').data("scrollpos");
-		var scrollOffset = 0;
-
-		if (nextTitle == true) {
-			next = $('#playList li.playing').next();
-			if (next.length == 0) {
-				next = $('#playList li').first();
-				scrollPos = 0;
-				// prevent of restarting the playlist when reached the end and settings are setted
-				if (repeatOnEnd !== undefined && repeatOnEnd == false) {
-					playNextTrack = false;
-				}
-			} else {
-				scrollPos += $(next).outerHeight(true);
-			}
-		} else {
-			next = $('#playList li.playing').prev();
-			if (next.length == 0) {
-				next = $('#playList li').last();
-				scrollPos = $('#playList').height() - $(next).outerHeight(true);
-			} else {
-				scrollPos -= $('#playList li.playing').outerHeight(true);
-			}
-		}
-
-		// hide all progress bars
-		$('#playList li.playing .playtime').text('00:00');
-		$('#playList li.playing .playprogress').css("width", "0%");
-		$('#playList li.playing').removeClass('playing');
-		$(next).addClass('playing');
-
-		// scroll the container
-		$('#playListWrapper').scrollTop(scrollPos)
-		$('#playListWrapper').data("scrollpos", scrollPos);
-
-		this.loadTrack(playNextTrack);
-	}
-
-	this.updatePercentage = function(percentage) {
-		if ($('#playList li.playing .duration').html() == "") {
-			$('#playList li.playing .duration').html(Tools.readableDuration(this.audioJs.element.duration));
-		}
-
-		var playedString = Tools.readableDuration(this.audioJs.duration * percentage);
-		var percent = Math.round(percentage * 100);
-		$('#playList li.playing .playprogress').css("width", percent + "%");
-		$('#playList li.playing .playtime').text(playedString);
-	}
-
-	this.stop = function() {
-		if (this.audioJs.playing == true) {
-			this.audioJs.pause();
-		}
-	}
 
 	this.fwd = function(fwd, eventCounter) {
 		var newVal = that.audioJs.element.currentTime;
--- frontend/scripts/player/menuitems/PlayerMenuItem.js
+++ frontend/scripts/player/menuitems/PlayerMenuItem.js
@@ -1,33 +1,43 @@
 var PlayerMenuItem = function(audioFile, parentId) {
-	this.audioFile = audioFile;
-	this.streamUrl = Config.baseUrl + '/sources/' + parentId.split('?').join('%3F');
-	this.id = parentId + "_player";
-	this.title = "Play: " + audioFile.name;
-	this.thumb = audioFile.thumb;
+  this.audioFile = audioFile;
+  this.streamUrl = Config.baseUrl + '/sources/' + parentId.split('?').join('%3F');
+  this.id = parentId + "_player";
+  this.title = "Play: " + audioFile.name;
+  this.thumb = audioFile.thumb;
 
-	this.displayContent = function() {
-		MenuTools.displayMenuItem(this);
-		AudioPlayer.loadTrack(this.streamUrl);
-	}
+  this.displayContent = function() {
+    MenuTools.displayContent("player", {
+      "menuItem": this,
+      "menuStack": MenuHandler.menuStack,
+      "position": MenuHandler.currentMenuIdx + 1,
+      "itemsCount": MenuHandler.currentMenuItems.length
+    });
 
-	this.handleKeyEvent = function(actionType) {
-		if (actionType == "left") {
-			// todo fwwd
-			MenuHandler.displayNextItem(false);
-		}
-		if (actionType == "right") {
-			// todo bkwd
-			MenuHandler.displayNextItem(true);
-		}
-		if (actionType == "action") {
-			AudioPlayer.audioJs.playPause();
-		}
+    AudioPlayer.loadTrack(this.streamUrl);
+  }
 
-		if (actionType == "back") {
-			AudioPlayer.stop();
-			this.performBack();
-		}
-	}
+  this.handleKeyEvent = function(actionType) {
+    if (actionType == "left") {
+      // todo fwwd
+      MenuHandler.displayNextItem(false);
+    }
+    if (actionType == "right") {
+      // todo bkwd
+      MenuHandler.displayNextItem(true);
+    }
+    if (actionType == "action") {
+      AudioPlayer.audioJs.playPause();
+    }
+
+    if (actionType == "back") {
+      $('#playerDuration').text("");
+      $('#playerPlaytime').text('00:00');
+
+      AudioPlayer.stop();
+      this.performBack();
+
+    }
+  }
 
 
 }
