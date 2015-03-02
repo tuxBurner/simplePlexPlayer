91ea00aad4c66ce45449a6bba388e52d74563733
Sebastian Hardt <tuxburner@boggo.de>
2015-03-03 00:29:07 +0100

    Added file thumb

--- backend/lib/sources/LocalSource.js
+++ backend/lib/sources/LocalSource.js
@@ -9,67 +9,67 @@ var AudioFile = require('./../AudioFile.js');
  */
 function LocalSource(conf) {
 
-	var that = this;
-	this.conf = conf;
-	this.rootFolder = new Folder(conf.name, conf.path);
+  var that = this;
+  this.conf = conf;
+  this.rootFolder = new Folder(conf.name, conf.path);
 
-	/**
-	 * This is called after this source is constructed
-	 */
-	this.loadSource = function() {
-		console.log("Adding local dir: " + conf.name + " : " + conf.path)
-		if (fs.existsSync(conf.path) == false) {
-			throw new Error("Local dir: " + conf.name + " does not exist");
-		}
-		this.handleDir(that.rootFolder);
-	}
+  /**
+   * This is called after this source is constructed
+   */
+  this.loadSource = function() {
+    console.log("Adding local dir: " + conf.name + " : " + conf.path)
+    if (fs.existsSync(conf.path) == false) {
+      throw new Error("Local dir: " + conf.name + " does not exist");
+    }
+    this.handleDir(that.rootFolder);
+  }
 
 
-	/**
-	 * reads a directory and adds the subfolders and mp3s
-	 */
-	this.handleDir = function(parentFolder, callBack) {
+  /**
+   * reads a directory and adds the subfolders and mp3s
+   */
+  this.handleDir = function(parentFolder, callBack) {
 
-		var parentPath = parentFolder.path;
+    var parentPath = parentFolder.path;
 
-		var dirContent = fs.readdirSync(parentPath);
+    var dirContent = fs.readdirSync(parentPath);
 
-		for (idx in dirContent) {
-			var dirContentName = dirContent[idx];
-			var absolutePath = parentPath + path.sep + dirContent[idx];
-			// check what type it is
-			var isDir = fs.lstatSync(absolutePath).isDirectory()
+    for (idx in dirContent) {
+      var dirContentName = dirContent[idx];
+      var absolutePath = parentPath + path.sep + dirContent[idx];
+      // check what type it is
+      var isDir = fs.lstatSync(absolutePath).isDirectory()
 
-			if (isDir == true) {
-				var subFolder = new Folder(dirContentName, absolutePath);
+      if (isDir == true) {
+        var subFolder = new Folder(dirContentName, absolutePath);
 
-				// register callback when the user requests this folder
-				subFolder.dataCallBack = function(httpRespCallback) {
-					that.handleDir(this, httpRespCallback);
-				}
+        // register callback when the user requests this folder
+        subFolder.dataCallBack = function(httpRespCallback) {
+          that.handleDir(this, httpRespCallback);
+        }
 
-				// disable caching if wished
-				if (this.conf.disableCaching === true) {
-					subFolder.disableCaching = true;
-				}
+        // disable caching if wished
+        if (this.conf.disableCaching === true) {
+          subFolder.disableCaching = true;
+        }
 
-				parentFolder.addSubFolder(subFolder);
-   			} else {
+        parentFolder.addSubFolder(subFolder);
+      } else {
 
-				var fileExtension = path.extname(dirContentName).toLowerCase();
+        var fileExtension = path.extname(dirContentName).toLowerCase();
 
-				// check if it is an image so we can add it as thumb
-				if (parentFolder.thumb === undefined && (fileExtension == ".jpeg" || fileExtension == ".jpg")) {
-					parentFolder.thumb = absolutePath + "/" + dirContentName;
-				}
+        // check if it is an image so we can add it as thumb
+        if (parentFolder.thumb === undefined && (fileExtension == ".jpeg" || fileExtension == ".jpg")) {
+          parentFolder.thumb = 'stream;' + absolutePath;
+        }
 
-				// check if we have a mp3
-				if (fileExtension != ".mp3") {
-					continue;
-				}
+        // check if we have a mp3
+        if (fileExtension != ".mp3") {
+          continue;
+        }
 
-				parentFolder.addFile(new AudioFile(dirContentName, absolutePath));
-				/*var stream = fs.createReadStream(absolutePath);
+        parentFolder.addFile(new AudioFile(dirContentName, absolutePath));
+        /*var stream = fs.createReadStream(absolutePath);
 				var parser = mm(stream, {duration: true});
 				parser.on('duration', function (result) {
 				  //console.log(result);
@@ -78,18 +78,24 @@ function LocalSource(conf) {
 				parser.on('done', function (err) {
 				  stream.destroy();
 				});*/
-			}
-		}
+      }
+    }
 
-		if (callBack !== undefined) {
-			callBack();
-		}
-		parentFolder.initialized = false;
-	}
+    if (parentFolder.thumb !== undefined) {
+      for (idx in parentFolder.audioFiles) {
+        parentFolder.audioFiles[idx].thumb = parentFolder.thumb;
+      }
+    }
+
+    if (callBack !== undefined) {
+      callBack();
+    }
+    parentFolder.initialized = false;
+  }
 
 
-	// initialize the root folder when this is constructed
-	that.loadSource();
+  // initialize the root folder when this is constructed
+  that.loadSource();
 }
 
 module.exports = LocalSource;
--- backend/server.js
+++ backend/server.js
@@ -6,8 +6,8 @@ var conf = require('./config.json');
 
 var rpiGpio = null;
 if (conf.displayOnOfPin !== undefined) {
-  var RpiGpio = require('./lib/RpiGpio.js');
-  rpiGpio = new RpiGpio(conf);
+	var RpiGpio = require('./lib/RpiGpio.js');
+	rpiGpio = new RpiGpio(conf);
 }
 
 var sources = {};
@@ -18,31 +18,31 @@ var RadioSource = require('./lib/sources/RadioSource.js');
 
 // load all sources and instanstiate them
 for (idx in conf.sources) {
-  var sourceConf = conf.sources[idx];
+	var sourceConf = conf.sources[idx];
 
-  var source = null;
+	var source = null;
 
-  switch (sourceConf.type) {
-    case "dir":
-    {
-      source = new LocalSource(sourceConf);
-      break;
-    }
-    case "plex":
-    {
-      source = new PlexSource(sourceConf);
-      break;
-    }
-    case "radio":
-    {
-      source = new RadioSource(sourceConf);
-      break;
-    }
-  }
+	switch (sourceConf.type) {
+		case "dir":
+			{
+				source = new LocalSource(sourceConf);
+				break;
+			}
+		case "plex":
+			{
+				source = new PlexSource(sourceConf);
+				break;
+			}
+		case "radio":
+			{
+				source = new RadioSource(sourceConf);
+				break;
+			}
+	}
 
-  if (source != null) {
-    sources[source.conf.name] = source;
-  }
+	if (source != null) {
+		sources[source.conf.name] = source;
+	}
 }
 
 
@@ -54,64 +54,73 @@ var app = express();
 
 // only expose gpio stuff when having gpio
 if (rpiGpio !== null) {
-  app.get('/display/on', function (req, res) {
-    rpiGpio.turnDisplayOn(res);
-  });
+	app.get('/display/on', function(req, res) {
+		rpiGpio.turnDisplayOn(res);
+	});
 
-  app.get('/display/off', function (req, res) {
-    rpiGpio.turnDisplayOff(res);
-  });
+	app.get('/display/off', function(req, res) {
+		rpiGpio.turnDisplayOff(res);
+	});
 }
 
 /**
  * #### SOURCES ENDPOINTS ####
  */
 // get the sources
-app.get('/sources', function (req, res) {
-  res.jsonp(Object.keys(sources));
+app.get('/sources', function(req, res) {
+	res.jsonp(Object.keys(sources));
 });
 
-app.get('/sources/:sourceName', function (req, res) {
-  if (sources[req.params.sourceName] === undefined) {
-    res.status(500).send('Source ' + req.params.sourceName + " not found !");
-  } else {
-    res.jsonp(sources[req.params.sourceName].rootFolder);
-  }
+app.get('/sources/:sourceName', function(req, res) {
+	if (sources[req.params.sourceName] === undefined) {
+		res.status(500).send('Source ' + req.params.sourceName + " not found !");
+	} else {
+		res.jsonp(sources[req.params.sourceName].rootFolder);
+	}
 });
 
-app.get('/sources/:sourceName/*', function (req, res) {
-  if (sources[req.params.sourceName] === undefined) {
-    res.status(500).send('Source ' + req.params.sourceName + " not found !");
-  } else {
-    var pathParts = req.params[0].split('/');
+app.get('/sources/:sourceName/*', function(req, res) {
+	if (sources[req.params.sourceName] === undefined) {
+		res.status(500).send('Source ' + req.params.sourceName + " not found !");
+	} else {
+		var pathParts = req.params[0].split('/');
 
-    var parent = sources[req.params.sourceName].rootFolder;
+		var parent = sources[req.params.sourceName].rootFolder;
 
-    for (idx in pathParts) {
-      var pathInfo = pathParts[idx];
-      if (parent.subFolders[pathInfo] === undefined) {
-        if (parent.audioFiles[pathInfo] === undefined) {
-          res.status(500).send('Source ' + pathInfo + " not found !");
-        } else {
-          var file = parent.audioFiles[pathInfo];
-          if (file.stream == false) {
-            res.sendFile(file.path);
-            return;
-          } else {
-            res.redirect(file.path);
-            return;
-          }
-        }
-      } else {
-        parent = parent.subFolders[pathInfo];
-      }
-    }
+		for (idx in pathParts) {
+			var pathInfo = pathParts[idx];
+			if (parent.subFolders[pathInfo] === undefined) {
+				if (parent.audioFiles[pathInfo] === undefined) {
+					res.status(500).send('Source ' + pathInfo + " not found !");
+				} else {
+					var file = parent.audioFiles[pathInfo];
+					if (file.stream == false) {
+						res.sendFile(file.path);
+						return;
+					} else {
+						res.redirect(file.path);
+						return;
+					}
+				}
+			} else {
+				parent = parent.subFolders[pathInfo];
+			}
+		}
 
-    parent.loadSubData(function () {
-      res.jsonp(parent);
-    });
+		parent.loadSubData(function() {
+			res.jsonp(parent);
+		});
 
-  }
+	}
+});
+
+app.get('/file/*', function(req, res) {
+	var path = req.params[0];
+	if (fs.existsSync(path) == false) {
+		res.status(500).send("Local file: " + path + " does not exist");
+	} else {
+		res.sendFile(path);
+	}
 });
 
 
@@ -123,52 +132,52 @@ var os = require('os');
 /**
  * Gather the network infos
  */
-var gatherNetDevInfos = function () {
-  var ifaces = os.networkInterfaces();
-  var iDevs = [];
-  for (var dev in ifaces) {
-    var alias = 0;
-    ifaces[dev].forEach(function (details) {
-      if (details.family == 'IPv4' && details.internal == false) {
-        var devName = dev + (alias ? ':' + alias : '');
-        iDevs.push({
-          "name": devName,
-          "details": details
-        });
-        ++alias;
-      }
-    });
-  }
-  return iDevs;
+var gatherNetDevInfos = function() {
+	var ifaces = os.networkInterfaces();
+	var iDevs = [];
+	for (var dev in ifaces) {
+		var alias = 0;
+		ifaces[dev].forEach(function(details) {
+			if (details.family == 'IPv4' && details.internal == false) {
+				var devName = dev + (alias ? ':' + alias : '');
+				iDevs.push({
+					"name": devName,
+					"details": details
+				});
+				++alias;
+			}
+		});
+	}
+	return iDevs;
 }
 
 /**
  * collects all the sysinfos
  */
-var gatherSysInfos = function () {
+var gatherSysInfos = function() {
 
-  // check if is in ap mode or not
-  var inApMode = fs.existsSync('./network/apMode');
+	// check if is in ap mode or not
+	var inApMode = fs.existsSync('./network/apMode');
 
 
-  var data = fs.readFileSync('./network/networkConf.json', 'utf8');
-  var netCfg = JSON.parse(data);
+	var data = fs.readFileSync('./network/networkConf.json', 'utf8');
+	var netCfg = JSON.parse(data);
 
-  var sysInfos = {
-    "ifaces": gatherNetDevInfos(),
-    "netCfg" : netCfg,
-    "inApMode": inApMode.toString()
-  };
+	var sysInfos = {
+		"ifaces": gatherNetDevInfos(),
+		"netCfg": netCfg,
+		"inApMode": inApMode.toString()
+	};
 
-  return sysInfos;
+	return sysInfos;
 
 }
 
 /**
  * Gathe the sys informations
  */
-app.get('/sys/infos', function (req, res) {
-  res.jsonp(gatherSysInfos());
+app.get('/sys/infos', function(req, res) {
+	res.jsonp(gatherSysInfos());
 });
 
 var fs = require('fs');
@@ -177,49 +186,52 @@ var exec = require('child_process').exec;
 /**
  * writes the network conf
  */
-app.get('/sys/network/config', function (req, res) {
-  fs.readFile('./network/networkConf.tpl', 'utf8', function (err, data) {
-    // replace the place holders
-    var result = data.replace('<ssidGoesHere>', req.query.ssid);
-    result = result.replace('<wpaGoesHere>', req.query.wpa);
+app.get('/sys/network/config', function(req, res) {
+	fs.readFile('./network/networkConf.tpl', 'utf8', function(err, data) {
+		// replace the place holders
+		var result = data.replace('<ssidGoesHere>', req.query.ssid);
+		result = result.replace('<wpaGoesHere>', req.query.wpa);
 
-    var cfg = {"ssid" :  req.query.ssid, "wpa":  req.query.wpa};
+		var cfg = {
+			"ssid": req.query.ssid,
+			"wpa": req.query.wpa
+		};
 
-    fs.writeFile('./network/networkConf.json', JSON.stringify(cfg) , function (err) {
-      // write the file
-      fs.writeFile('./network/networkConf.cfg', result, function (err) {
-        if (!err) {
-          execStopApMode(res);
-        }
-      });
-    });
-  });
+		fs.writeFile('./network/networkConf.json', JSON.stringify(cfg), function(err) {
+			// write the file
+			fs.writeFile('./network/networkConf.cfg', result, function(err) {
+				if (!err) {
+					execStopApMode(res);
+				}
+			});
+		});
+	});
 });
 
 /**
  * Starts the ap mode
  */
-app.get('/sys/network/apMode/start', function (req, res) {
-  execStartApMode(res);
+app.get('/sys/network/apMode/start', function(req, res) {
+	execStartApMode(res);
 });
 
 /**
  * Stops the ap mode
  */
-app.get('/sys/network/apMode/stop', function (req, res) {
-  execStopApMode(res);
+app.get('/sys/network/apMode/stop', function(req, res) {
+	execStopApMode(res);
 });
 
 /**
  * Switch the ap mode
  */
-app.get('/sys/network/apMode/switch', function (req, res) {
-  var inApMode = fs.existsSync('./network/apMode');
-  if(inApMode == true) {
-    execStopApMode(res);
-  } else {
-    execStartApMode(res);
-  }
+app.get('/sys/network/apMode/switch', function(req, res) {
+	var inApMode = fs.existsSync('./network/apMode');
+	if (inApMode == true) {
+		execStopApMode(res);
+	} else {
+		execStartApMode(res);
+	}
 });
 
 
@@ -227,55 +239,61 @@ app.get('/sys/network/apMode/switch', function (req, res) {
  * Stops the ap mode of the machine and starts normal networking
  * @param res
  */
-var execStopApMode = function (res) {
-  exec("./network/stopApMode.sh " + conf.networkCfgFile, function (error, stdout, stderr) {
-    res.jsonp({"status": "okay"});
-  });
+var execStopApMode = function(res) {
+	exec("./network/stopApMode.sh " + conf.networkCfgFile, function(error, stdout, stderr) {
+		res.jsonp({
+			"status": "okay"
+		});
+	});
 };
 
 /**
  * Stops the ap mode of the machine and starts normal networking
  * @param res
  */
-var execStartApMode = function (res) {
-  exec("./network/startApMode.sh " + conf.networkCfgFile, function (error, stdout, stderr) {
-    res.jsonp({"status": "okay"});
-  });
+var execStartApMode = function(res) {
+	exec("./network/startApMode.sh " + conf.networkCfgFile, function(error, stdout, stderr) {
+		res.jsonp({
+			"status": "okay"
+		});
+	});
 };
 
 /**
  * This can be called to press a key in x
  */
-app.get('/sys/key/:key', function (req, res) {
-  pressKeyInX(req.params.key, res);
+app.get('/sys/key/:key', function(req, res) {
+	pressKeyInX(req.params.key, res);
 });
 
 /**
  * Presses the given key in the x window
  * @param key
  */
-var pressKeyInX = function (key, res) {
-  exec('./pressXKey.sh '+key, function (error, stdout, stderr) {
-    res.jsonp({"status": "okay"});
-  });
+var pressKeyInX = function(key, res) {
+	exec('./pressXKey.sh ' + key, function(error, stdout, stderr) {
+		res.jsonp({
+			"status": "okay"
+		});
+	});
 }
 
 
-var server = app.listen(conf.serverPort, function () {
-  console.log('Listening on port %d', server.address().port);
+var server = app.listen(conf.serverPort, function() {
+	console.log('Listening on port %d', server.address().port);
 });
 
 
 /**
  * #### HANDLE SHUTDOWN ####
  */
-process.on('SIGINT', function () {
-  console.log("\nGracefully shutting down from SIGINT (Ctrl-C)");
-  // some other closing procedures go here
+process.on('SIGINT', function() {
+	console.log("\nGracefully shutting down from SIGINT (Ctrl-C)");
+	// some other closing procedures go here
 
-  if (rpiGpio !== null) {
-    rpiGpio.close();
-  } else {
-    process.exit();
-  }
+	if (rpiGpio !== null) {
+		rpiGpio.close();
+	} else {
+		process.exit();
+	}
 });
