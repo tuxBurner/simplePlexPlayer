0de3868daf785f54beb088ef4c7e586940f4909f
Sebastian Hardt <tuxburner@boggo.de>
2015-03-07 09:53:33 +0100

    Made a general menu item

--- frontend/scripts/player/menuitems/options/OptionSysInfoMenuItem.js
+++ frontend/scripts/player/menuitems/options/OptionSysInfoMenuItem.js
@@ -3,27 +3,23 @@
  */
 var OptionSysInfoMenuItem = function() {
 
-	var that = this;
+  var that = this;
 
-	this.title = 'System Info';
-	this.id = 'options_sysinfos';
+  this.title = 'System Info';
+  this.id = 'options_sysinfos';
+  this.cssClass = 'options';
 
-	this.performAction = function() {
-		return;
-	}
+  this.performAction = function() {
+    return;
+  }
 
-	this.displayContent = function() {
-		Tools.callBackend(Config.baseUrl + '/sys/infos', function(data) {
-			MenuTools.displayContent('options_wrapper', {
-				"title": that.title,
-				"menuStack": MenuHandler.menuStack,
-				"position": MenuHandler.currentMenuIdx + 1,
-				"itemsCount": MenuHandler.currentMenuItems.length,
-				"optionsContent": MenuTools.handleBarTpls['options_sysinfos']({
-					"data": data
-				})
-			});
-		});
-	}
+  this.displayContent = function() {
+    Tools.callBackend(Config.baseUrl + '/sys/infos', function(data) {
+      var sysInfoContent = MenuTools.handleBarTpls['options_sysinfos']({
+        "data": data
+      });
+      MenuTools.displayMenuItem(that, sysInfoContent);
+    });
+  }
 }
 OptionSysInfoMenuItem.prototype = new MenuItem;
--- frontend/scripts/player/menuitems/options/OptionWifiSettingsMenuItem.js
+++ frontend/scripts/player/menuitems/options/OptionWifiSettingsMenuItem.js
@@ -3,78 +3,75 @@
  */
 var OptionWifiSettingsMenuItem = function() {
 
-	var that = this;
+  var that = this;
 
-	this.title = 'Display Wifi Settings';
-	this.id = 'options_wifisettings';
+  this.title = 'Display Wifi Settings';
+  this.id = 'options_wifisettings';
 
-	this.performAction = function() {
-		return;
-	}
+  this.performAction = function() {
+    return;
+  }
 
 
-	this.handleKeyEvent = function(actionType) {
-		if (actionType == "action") {
-			var elemId = $(document.activeElement).attr('id');
+  this.handleKeyEvent = function(actionType) {
+    if (actionType == "action") {
+      var elemId = $(document.activeElement).attr('id');
 
-			// submit the data to the backend
-			if (elemId == 'options_wifisettings_submit') {
-				var ssid = $('#options_wifisettings_essid').val();
-				var wpa = $('#options_wifisettings_wpa').val();
+      // submit the data to the backend
+      if (elemId == 'options_wifisettings_submit') {
+        var ssid = $('#options_wifisettings_essid').val();
+        var wpa = $('#options_wifisettings_wpa').val();
 
-				var url = '/sys/network/config?wpa=' + wpa + '&ssid=' + ssid;
+        var url = '/sys/network/config?wpa=' + wpa + '&ssid=' + ssid;
 
-				Tools.callBackend(Config.baseUrl + url, function(data) {
-					MenuHandler.loadParentContent();
-				});
-				return;
-			}
+        Tools.callBackend(Config.baseUrl + url, function(data) {
+          MenuHandler.loadParentContent();
+        });
+        return;
+      }
 
-			// switch the mode of the ap
-			if (elemId == 'options_wifisettings_switchapmode') {
-				Tools.callBackend(Config.baseUrl + '/sys/network/apMode/switch',
-					function(data) {
-						window.location.reload();
-					});
-				return;
-			}
+      // switch the mode of the ap
+      if (elemId == 'options_wifisettings_switchapmode') {
+        Tools.callBackend(Config.baseUrl + '/sys/network/apMode/switch',
+          function(data) {
+            window.location.reload();
+          });
+        return;
+      }
 
-			if (elemId == 'options_wifisettings_cancel') {
-				MenuHandler.loadParentContent();
-				return;
-			}
-		}
+      if (elemId == 'options_wifisettings_cancel') {
+        MenuHandler.loadParentContent();
+        return;
+      }
+    }
 
-		// set the focus on the next input when back action is hit
-		if (actionType == "back") {
-			Tools.nextOnTabIndex($(document.activeElement)).focus();
-			return;
-		}
-	}
+    // set the focus on the next input when back action is hit
+    if (actionType == "back") {
+      Tools.nextOnTabIndex($(document.activeElement)).focus();
+      return;
+    }
+  }
 
-	this.displayContent = function() {
-		Tools.callBackend(Config.baseUrl + '/sys/infos', function(data) {
-			MenuTools.displayContent('options_wrapper', {
-				"title": that.title,
-				"menuStack": MenuHandler.menuStack,
-				"position": MenuHandler.currentMenuIdx + 1,
-				"itemsCount": MenuHandler.currentMenuItems.length,
-				"optionsContent": MenuTools.handleBarTpls['options_wifisettings']({
-					"data": data
-				})
-			});
-			var simpleScreenOptions = {
-				"keys": {
-					39: "nextChar",
-					37: "prevChar",
-					27: "cancle",
-					13: "enter"
-				}
-			}
-			$('#options_wifisettings_essid').simpleOnScreenKeyb(simpleScreenOptions);
-			$('#options_wifisettings_wpa').simpleOnScreenKeyb(simpleScreenOptions);
-			Tools.nextOnTabIndex($(document.activeElement)).focus();
-		});
-	}
+  this.displayContent = function() {
+    Tools.callBackend(Config.baseUrl + '/sys/infos', function(data) {
+
+      var optionsSettings = MenuTools.handleBarTpls['options_wifisettings']({
+        "data": data
+      });
+      MenuTools.displayMenuItem(that, optionsSettings);
+
+      var simpleScreenOptions = {
+        "keys": {
+          39: "nextChar",
+          37: "prevChar",
+          27: "cancle",
+          13: "enter"
+        }
+      }
+      $('#options_wifisettings_essid').simpleOnScreenKeyb(simpleScreenOptions);
+      $('#options_wifisettings_wpa').simpleOnScreenKeyb(simpleScreenOptions);
+      Tools.nextOnTabIndex($(document.activeElement)).focus();
+    });
+  }
 }
 OptionWifiSettingsMenuItem.prototype = new MenuItem;
