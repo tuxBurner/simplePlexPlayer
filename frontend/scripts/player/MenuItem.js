f0c7a8f8721e0bfd2f27aea6ffab3345220ed1a5
Sebastian Hardt <tuxburner@boggo.de>
2015-03-07 11:52:17 +0100

    Some tiny performance tuning

--- frontend/scripts/player/MenuItem.js
+++ frontend/scripts/player/MenuItem.js
@@ -2,44 +2,44 @@
  * This is the simpliest menu item we can have
  */
 var MenuItem = function() {
-	this.title = "No title set";
-	this.id = "No id set";
-	this.cssClass = "";
+  this.title = "No title set";
+  this.id = "No id set";
+  this.cssClass = "";
 
-	// normal handling of actions take place here
-	this.handleKeyEvent = function(actionType, keyWasDown) {
+  // normal handling of actions take place here
+  this.handleKeyEvent = function(actionType, keyWasDown) {
 
-		if (actionType == "left") {
-			MenuHandler.displayNextItem(false);
-		}
-		if (actionType == "right") {
-			MenuHandler.displayNextItem(true);
-		}
-		if (actionType == "action") {
-			this.performAction();
-		}
+    if (actionType == "left" && MenuHandler.currentMenuItems.length > 1) {
+      MenuHandler.displayNextItem(false);
+    }
+    if (actionType == "right" && MenuHandler.currentMenuItems.length > 1) {
+      MenuHandler.displayNextItem(true);
+    }
+    if (actionType == "action") {
+      this.performAction();
+    }
 
-		if (actionType == "back") {
-			this.performBack();
-		}
-	}
+    if (actionType == "back") {
+      this.performBack();
+    }
+  }
 
-	this.handleKeyEventDown = function(actionType, keyDownCounter) {}
+  this.handleKeyEventDown = function(actionType, keyDownCounter) {}
 
-	this.performAction = function() {
-		alert("Override me performAction");
-	}
+  this.performAction = function() {
+    alert("Override me performAction");
+  }
 
-	this.performBack = function() {
-		MenuHandler.loadParentContent();
-	}
+  this.performBack = function() {
+    MenuHandler.loadParentContent();
+  }
 
-	// this is called when the menu item is displaying its content
-	this.displayContent = function() {
-		alert("Override me displayContent !");
-	}
+  // this is called when the menu item is displaying its content
+  this.displayContent = function() {
+    alert("Override me displayContent !");
+  }
 
-	this.loadSubMenuItems = function(highlightId) {
-		alert("Override me loadSubMenuItems !");
-	}
+  this.loadSubMenuItems = function(highlightId) {
+    alert("Override me loadSubMenuItems !");
+  }
 }
