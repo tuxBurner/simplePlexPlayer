aae84906c49a0bfa00535db210eeee9054d3d397
Sebastian Hardt <tuxburner@boggo.de>
2015-03-08 12:28:27 +0100

    Fixed buggy screen on off handling

--- frontend/scripts/main.js
+++ frontend/scripts/main.js
@@ -2,27 +2,27 @@ var player = null;
 
 
 var Config = {
-	// path to the the backend server
-	"baseUrl": "http://localhost:8080",
-	"repeatAll": false,
-	"skipSeconds": {
-		"slow": 10,
-		"fast": 20,
-		"fastTrigger": 10
-	},
-	"keyMapping": {
-		"left": 37,
-		"right": 39,
-		"back": 27,
-		"action": 13
-	},
-	"rotaryInput": true,
-	"eventTimeOut": 250,
-	"rotaryEventTimeout": 250,
-	"sleepTimeOut": 60
+  // path to the the backend server
+  "baseUrl": "http://localhost:8080",
+  "repeatAll": false,
+  "skipSeconds": {
+    "slow": 10,
+    "fast": 20,
+    "fastTrigger": 10
+  },
+  "keyMapping": {
+    "left": 37,
+    "right": 39,
+    "back": 27,
+    "action": 13
+  },
+  "rotaryInput": true,
+  "eventTimeOut": 250,
+  "rotaryEventTimeout": 250,
+  "sleepTimeOut": 120
 }
 
 $(function() {
-	player = new Player();
-	player.init();
+  player = new Player();
+  player.init();
 })
