1987bfcabb240abb738c453394a0787869d076a3
Sebastian Hardt <tuxburner@boggo.de>
2015-03-10 09:13:00 +0100

    Config for frontend is now an own file

---
+++ frontend/scripts/player/Config.js
@@ -0,0 +1,21 @@
+// the config
+var Config = {
+  // path to the the backend server
+  "baseUrl": "http://localhost:8080",
+  "repeatAll": false,
+  "skipSeconds": {
+    "slow": 10,
+    "fast": 20,
+    "fastTrigger": 10
+  },
+  "keyMapping": {
+    "left": 37,
+    "right": 39,
+    "back": 27,
+    "action": 13
+  },
+  "rotaryInput": true,
+  "eventTimeOut": 250,
+  "rotaryEventTimeout": 250,
+  "sleepTimeOut": 120
+}
