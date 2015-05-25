f2fb1cd5e2ef0b4d745976fec34f009bc7579ab4
Sebastian Hardt <tuxburner@boggo.de>
2015-05-25 20:23:27 +0200

    made bower behave diffrent

--- backend/Gruntfile.js
+++ backend/Gruntfile.js
@@ -11,7 +11,7 @@ module.exports = function(grunt) {
     bower: {
       install: {
         options: {
-          "targetDir": "./views/public"
+          "targetDir": "./views/public/bower"
         }
       }
     }
